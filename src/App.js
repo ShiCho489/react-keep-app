c928e4abab609b7d6520dd31856f75c41ac49ed7
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-05-24 10:47:52 +0300

    server started

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
 import CreateNote from './components/CreateNote';
-{/*import notes from './notes';*/}
 
 
 
 
 
 function App() {
+
   return (
     <div className="App">
       <Header />
-      {/*{notes.map((noteItem) => (
-       <Note 
-       key= {noteItem.key}
-       title={noteItem.title}
-       content={noteItem.content}
-      />
-      ))}*/}
+      
+      
       <CreateNote />
       
       <Footer />

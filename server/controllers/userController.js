56b6ca659d6bda32a0b80424345905833a6f5ac5
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-06-05 15:26:26 +0300

    user-registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,45 +1,42 @@
+import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
-
-import User from '../models/userModels'
 
 
 const registerUser = asyncHandler(async(req, res) => {
     const { name, email, password, pic } =req.body;
 
-    const userExits = await User.findOne({ email })
+    const userExists = await User.findOne({ email });
 
-    if(userExits) {
+    if(userExists) {
         res.status(400);
-        throw new Error("User already exists");
+        throw new Error("User already exists")
     }
-
-
+  
     const user = await User.create({
         name,
         email,
         password,
-        picture,
+        pic,
     })
 
-    if (user) {
+    if(user) {
         res.status(201).json({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          isAdmin: user.isAdmin,
-          pic: user.pic,
-          token: generateToken(user._id),
-        });
-      } else {
-        res.status(400);
-        throw new Error("User not found");
-      }
-    });
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            pic: user.pic,
+           
+        })
+        }else {
+            res.status(400);
+            throw new Error("User not found");
 
-    res.json ({
-        name,
-        email,
-    })
-})
+        }
+    }
+  )
 
-export default registerUser
+   
+    
+
+export default registerUser;

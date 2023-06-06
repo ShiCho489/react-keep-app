132d819023dfb648b1f4330e4965c5a9ff5bd834
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-06-06 10:26:37 +0300

    register page is done

--- client/src/screens/RegisterScreen/RegisterPage.js
+++ client/src/screens/RegisterScreen/RegisterPage.js
@@ -4,68 +4,72 @@ import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
 //import Loading from "../../components/Loading";
 //import ErrorMessage from "../../components/ErrorMessage";
+import "./RegisterPage.css";
 
 const RegisterPage = () => {
 
   return (
-    <MainScreen title="REGISTER">
-    <div className= "loginContainer">
-   <Form>
-        <Form.Group controlId ="name">
-          <Form.Label>
-            name
+    <MainScreen title= "REGISTER">
+    <div className="loginContainer">
+    <Form >
+      <Form.Group controlId="formBasicEmail">
+      <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              //value={email}
+              placeholder="Enter name"
+             // onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
 
-          </Form.Label>
-           <Form.Control
-           type="name"
-
-           placeholder="Enter name"
-           >
-
-           </Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
             <Form.Control
               type="email"
-              //value={email}
-              placeholder="Enter email"
-              //onChange={(e) => setEmail(e.target.value)}
+             // value={password}
+              placeholder="Email"
+              //onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+             // value={password}
+              placeholder="Password"
+              //onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
           <Form.Group controlId="confirmPassword">
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
+             
               type="password"
-              //value={confirmpassword}
+             // value={password}
               placeholder="Confirm Password"
-              //onChange={(e) => setConfirmPassword(e.target.value)}
+              //onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
-
           <Form.Group controlId="pic">
-            <Form.Label>Profile Picture</Form.Label>
-            <Form.File
-              //onChange={(e) => postDetails(e.target.files[0])}
-              //id="custom-file"
-              //type="image/png"
-              //label="Upload Profile Picture"
-              //custom
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
+           <label for="formFile" class="form-label">Profile Picture</label>
+          <input class="form-control" type="file" id="formFile" />
+         </Form.Group>
+
+         <Button className="button" variant="primary" type="submit">
             Register
           </Button>
-      </Form>
-      <Row className="py-3">
+          </Form>
+          <Row className="py-3">
           <Col>
             Have an Account ? <Link to="/login">Login</Link>
           </Col>
         </Row>
-    </div>
-   </MainScreen>
+    
+      </div>
+    </MainScreen>
+    
   )
+    
 }
 
 export default RegisterPage

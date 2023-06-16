79948e58a6899a2674063f2fb4b28212e643e0dc
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-06-16 09:20:30 +0300

    provider from redux upgraded

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store'
 
 
+render(
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    
+    <Provider store = {store}>
     <App />
     
-   
-  </React.StrictMode>
+   </Provider>,
+   document.getElementById("root")
+  
 );
 
 

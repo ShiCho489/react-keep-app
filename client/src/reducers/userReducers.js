553fff0792c812c4ec139979d5cd528a1da852df
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-06-16 10:23:36 +0300

    redux-reducer

---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,10 @@
+export const userLoginReducer = (state={}, action) => {
+    switch (action.type) {
+        case USER_LOGIN_REQUEST: 
+
+        break;
+
+        default: 
+          break;
+    }
+}

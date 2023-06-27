3c8a3b477300781c58ba58380b0a32fb1c2b470e
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-06-27 09:33:00 +0300

    root

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,25 @@ import {BrowserRouter, Routes, Route } from "react-router-dom"
 import MyNotes from './screens/MyNotes'
 import LoginPage from './screens/LoginScreen/LoginPage'
 import RegisterPage from './screens/RegisterScreen/RegisterPage'
+import { useState } from "react";
 
 
 function App() {
+
+  const [search, setSearch] = useState("");
   return (
     <BrowserRouter>
-     <Header />
+     <Header setSearch={(s) => setSearch(s)}/>
      <main>
       
       <Routes>
       <Route path='/' element={<MainPage />} exact/>
       <Route path='/login' element={<LoginPage />} exact />
       <Route path='/register' element={<RegisterPage />} exact />
-       <Route path='/mynotes' element={<MyNotes />} />
+       <Route path='/mynotes' element={({ history }) => (
+        <MyNotes search={search} history={history}/> 
+       )}
+       />
 
 
       </Routes>

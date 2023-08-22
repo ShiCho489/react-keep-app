1ae52e7dab041040882cf3067787842a18bacb49
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-08-22 11:07:26 +0300

    reducers

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -4,7 +4,12 @@ USER_LOGIN_SUCCESS,
 USER_LOGOUT,
 USER_REGISTER_FAIL,
 USER_REGISTER_REQUEST,
-USER_REGISTER_SUCCESS,} from "../constants/userConstants";
+USER_REGISTER_SUCCESS,
+USER_UPDATE_REQUEST, 
+USER_UPDATE_SUCCESS, 
+USER_UPDATE_FAIL} from "../constants/userConstants";
+
+
 
 export const userLoginReducer = (state={}, action) => {
     switch (action.type) {
@@ -30,6 +35,19 @@ export const userRegisterReducer = (state = {}, action) => {
       return { loading: false, userInfo: action.payload };
     case USER_REGISTER_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_SUCCESS:
+      return { loading: false, userInfo: action.payload, success: true };
+    case USER_UPDATE_FAIL:
+      return { loading: false, error: action.payload, success: false };
     default:
       return state;
   }
